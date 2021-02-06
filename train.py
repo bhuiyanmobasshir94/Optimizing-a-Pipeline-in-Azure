2e03219578d665a8e9e49831c0f17141d1bea07a
Mobasshir Bhuiyan <mobasshirbhuiyan.shagor@gmail.com>
2021-02-06 17:31:33 +0000

    Update train.py

--- train.py
+++ train.py
@@ -14,17 +14,7 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = TabularDatasetFactory.from_delimited_files(["https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"])
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
-
+## customization for clean_data
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -50,7 +40,18 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+    return x_df, y_df
+
+
+ds = TabularDatasetFactory.from_delimited_files(["https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"])
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3)
+
+run = Run.get_context()
+
 
 def main():
     # Add arguments to script
@@ -64,9 +65,9 @@ def main():
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
 
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(X_train, y_train)
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
-    accuracy = model.score(X_test, y_test)
+    accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
 
 if __name__ == '__main__':
